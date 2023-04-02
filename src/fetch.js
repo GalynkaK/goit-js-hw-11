850931d12443e5b88c1eb873e7de1abac5493c42
GalynkaK <gkarpinskaya@gmail.com>
2023-04-02 15:07:50 +0300

    created

---
+++ src/fetch.js
@@ -0,0 +1,45 @@
+const axios = require('axios').default;
+const URL = 'https://pixabay.com/api/';
+const KEY = '34885088-d9d31bccec02718712fb034c5';
+
+export default class PixabayApiServise {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.per_page = 40;
+  }
+
+  async fetchPicture() {
+    const searchParams = new URLSearchParams({
+      key: KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: this.per_page,
+    });
+
+    const url = `${URL}?${searchParams}`;
+    this.incrementPage();
+    return await axios.get(url);
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get per_Page() {
+    return this.per_page;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
